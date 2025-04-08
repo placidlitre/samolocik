43112370470c62b60437035c69db3a84648d6561
Fertiv <oliwierwandycz1@gmail.com>
2025-04-08 17:53:26 +0200

    pog

--- samolick/samolick.cpp
+++ samolick/samolick.cpp
@@ -1,20 +1,114 @@
-﻿// samolick.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+using namespace std;
 
-#include <iostream>
+
+char MapaSamolotow[60][10];
+
+class Samolot
+{
+
+	int x;
+	int poziomY;
+	int zmianaLotu;
+	char litera;
+public:
+	Samolot() : litera('X') {}
+	Samolot(const char& literka, const int& poziomekY) : litera(literka), poziomY(poziomekY) {}
+
+	void zmianaY()
+	{
+		if (zmianaLotu < 0)
+		{
+			poziomY++;
+			zmianaLotu++;
+		}
+		else if (zmianaLotu > 0)
+		{
+			poziomY--;
+			zmianaLotu--;
+		}
+	}
+
+	void poziomDoGory(int zmianaLotku)
+	{
+		poziomY++;
+	}
+
+	void poziomDoDolu(int zmianaLotku)
+	{
+		poziomY--;
+	}
+
+	void wpisanieZmianyLotu(int zmianaLotku)
+	{
+		zmianaLotu = zmianaLotku;
+	}
+
+	void pokazSamolot()
+	{
+		cout << litera << endl;
+		cout << zmianaLotu << endl << endl;
+	}
+
+};
+
+void wpiszKomende()
+{
+
+}
+
+
+void Plansza()
+{
+	for (int i = 0; i < 62; i++)
+		cout << "=";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "|";
+		for (int j = 0; j < 62; j++)
+		{
+
+		}
+	}
+}
+
+void tworzenieSamolotu(vector<Samolot>& samoloty, int& licznikSamolotow)
+{
+	int liniaY = rand() % 10;
+	samoloty.emplace_back((char(66 + licznikSamolotow)), liniaY); //tworzy obiekt + odrazu działa konstruktor który wpisuje mu daną litere w liście inicjalizacyjnej
+	samoloty[licznikSamolotow].pokazSamolot();
+	licznikSamolotow++;
+}
+
+void kontrolaLotow(vector<Samolot>& samoloty)
+{
+	string komenda;
+	char szukanaLiteraSamolotu;
+
+	literaSamolotu = komenda[0];
+	auto it = find_if(samoloty.begin(), samoloty.end(), [szukanaLiteraSamolotu](Samolot& samolot)
+		{
+			return samolot.get
+		}
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(NULL));
+	vector <Samolot> samoloty;
+	int liczbaSamolotow = 0;
+
+
+	kontrolaLotow();
+
+
+	tworzenieSamolotu(samoloty, liczbaSamolotow);
+	tworzenieSamolotu(samoloty, liczbaSamolotow);
+
+
+
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
