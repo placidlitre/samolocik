c41f7045c913e5f05c4cb6096111921097fbd5a7
PlacidLitre <robstef@protonmail.com>
2025-04-16 07:17:21 +0200

    wstępna wersja mapy, zmieniono definicję zmianyY, nowa wersja warunku

--- samolick/samolick.cpp
+++ samolick/samolick.cpp
@@ -4,8 +4,10 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-
+#include <algorithm>
 using namespace std;
+const unsigned int WYS_PLANSZA = 10;
+const unsigned int SZER_PLANSZA = 60;
 
 class Samolot
 {
@@ -23,12 +25,12 @@ public:
 	{
 		if (zmianaLotu < 0)
 		{
-			poziomY--;
+			poziomY++;
 			zmianaLotu++;
 		}
 		else if (zmianaLotu > 0)
 		{
-			poziomY++;
+			poziomY--;
 			zmianaLotu--;
 		}
 	}
@@ -62,9 +64,9 @@ void tworzenieSamolotu(vector<Samolot>& samoloty, int& licznikSamolotow)
 {
 	int liniaY = rand() % 10;
 	int kierunek = rand() % 2;
-	if(kierunek==0)
+	if (kierunek == 0)
 		samoloty.emplace_back((char(65 + licznikSamolotow)), liniaY, 0, -1); // samoloty w prawo    tworzy obiekt + odrazu działa konstruktor który wpisuje mu daną litere w liście inicjalizacyjnej
-	else if(kierunek==1)
+	else if (kierunek == 1)
 		samoloty.emplace_back((char(65 + licznikSamolotow)), liniaY, 0, -12); // samoloty w lewo
 
 	samoloty[licznikSamolotow].pokazSamolot();
@@ -90,12 +92,12 @@ void kontrolaLotow(vector<Samolot>& samoloty, int liczbaSamolotow)
 		if (komenda[0] == ' ')
 			return;
 
-		if (int(komenda[0]) - 65 >= liczbaSamolotow || int(komenda[0]) - 65<0 || komenda.size() != 3)
+		if (int(komenda[0]) - 65 >= liczbaSamolotow || int(komenda[0]) - 65 < 0 || komenda.size() != 3)
 		{
 			cout << "Bledna komenda" << endl;
 			continue;
 		}
-		
+
 		if (komenda[1] == '/')
 		{
 			samoloty[indeks].wpisanieZmianyLotu(int(komenda[2]) - 48);
@@ -147,85 +149,70 @@ void tworzenieSamolotuNaMapie(vector<Samolot>& samoloty, int liczbaSamolotow, in
 
 		cout << "(" << samoloty[indeks].litera << abs(samoloty[indeks].zmianaLotu) << ")";
 
-		if (samoloty[indeks].x>0 && samoloty[indeks].zmianaLotu > 0)
+		if (samoloty[indeks].x > 0 && samoloty[indeks].zmianaLotu > 0)
 			cout << "/";
 		else if (samoloty[indeks].x > 0 && samoloty[indeks].zmianaLotu < 0)
 			cout << "\\";
 	}
 }
 
-void warunek(vector<Samolot>& samoloty, int liczbaSamolotow, int Mapa[][10])
+void warunek(vector<Samolot>& samoloty)
 {
-	int kierunekPoziomy[2] = { -2, 2 };
-
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 12; j++)
-		{
-			if (Mapa[j][i] != -1)
-			{
-				for (int k = 0; k < 2; k++)
-				{
-					if (j + kierunekPoziomy[k] < 0 || j + kierunekPoziomy[k]>11)
-						continue;
-
-					if (Mapa[j + kierunekPoziomy[k]][i] != -1)
-					{
-						cout << "GG";
-						exit(0);
-					}
-					
-				}
-			}
+	for (int i = 0; i < samoloty.size(); i++) {
+		for (int j = i + 1; j < samoloty.size(); j++) {
+			if (abs(samoloty[i].x - samoloty[j].x) <= 2 && abs(samoloty[i].poziomY - samoloty[j].poziomY) <= 2)exit(0);
 		}
 	}
 }
 
 void tworzenieMapy(vector<Samolot>& samoloty, int liczbaSamolotow)
 {
-	int Mapa[12][10];
-
-	int indeks=0;
-
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 12; j++)
-		{
-			Mapa[j][i] = -1;
-		}
-	}
-	
-	for (int i = 0; i < samoloty.size(); i++)
-	{
-		
-		Mapa[abs(samoloty[i].x)][9-samoloty[i].poziomY] = i;
-	}
-		
-		cout << " ";
-		for (int i = 0; i < 62; i++)
-			cout << "=";
-
-		cout << endl;
-		for (int i = 0; i < 10; i++)
-		{
-			cout << 9-i << "|";
-			for (int j = 0; j < 12; j++)
-			{
-				if (Mapa[j][i] != -1)
-				{
-					indeks = Mapa[j][i];
-					tworzenieSamolotuNaMapie(samoloty, liczbaSamolotow, indeks);
-				}
-				else
-					cout << "     ";
+	/*for (Samolot samolot : samoloty) {
+		cout << samolot.litera << ", poziom Y: " << samolot.poziomY << endl;
+	}*/
+	vector <vector<Samolot>> tablicaSamolotowWLinii;
+	tablicaSamolotowWLinii.resize(WYS_PLANSZA); // deklaruje ze vector bedzie mial 10 wierszy, potrzebne
+	for (int y = 0; y < WYS_PLANSZA; y++) {
+		for (Samolot samolot : samoloty) {
+			if (samolot.poziomY == y) {
+				tablicaSamolotowWLinii[y].push_back(samolot); // wpisuje do vectora[y] samoloty ktore sa na tej linii 
 			}
+		}
+		sort(tablicaSamolotowWLinii[y].begin(), tablicaSamolotowWLinii[y].end(), [](Samolot sam1, Samolot sam2) {
+			return abs(sam1.x) < abs(sam2.x);
+			}); // sortuje rosnaco na podstawie modulu koordynatow x 
+
+		/*for (Samolot samolot : tablicaSamolotowWLinii[y]) {
+			cout << samolot.x << " ";
+		} //wypisz vector 
+		cout << endl;*/
+		cout << "|";
+		if (tablicaSamolotowWLinii[y].size() == 0) {
+			for (int x = 0; x < SZER_PLANSZA; x++) cout << " ";
 			cout << "|" << endl;
 		}
-		cout << setw(3);
-		for (int i = 0; i < 12; i++)
-			cout << i << setw(5);
-		cout << endl;
-		warunek(samoloty, liczbaSamolotow, Mapa);
+		else {
+			int ileWypisanoPol = 0;
+			int xPoprzedniegoSamolotu = 0;
+
+			for (int i = 0; i < tablicaSamolotowWLinii[y].size();i++) {
+				for (int x = 0; x < abs(tablicaSamolotowWLinii[y][i].x)- xPoprzedniegoSamolotu; x++)
+					cout << " ";
+				tworzenieSamolotuNaMapie(samoloty, liczbaSamolotow, tablicaSamolotowWLinii[y][i].litera - 'A');
+				ileWypisanoPol += abs(tablicaSamolotowWLinii[y][i].x) + 5; //dlugosc 1 samolotu
+				xPoprzedniegoSamolotu = abs(tablicaSamolotowWLinii[y][i].x);
+
+			}
+			for (int i = 0; i < SZER_PLANSZA - ileWypisanoPol; i++) cout << " ";
+			cout << "|" << endl;
+			
+		}
+	}
+
+
+	
+
+
 
 }
 
@@ -243,7 +230,7 @@ void tura(vector<Samolot>& samoloty, int liczbaSamolotow)
 
 
 
- 
+
 //w oddzielnym watku thread tworzymy samoloty
 
 int main()
@@ -252,12 +239,12 @@ int main()
 	vector <Samolot> samoloty;
 	int liczbaSamolotow = 0;
 	int licznik = 0;
-	
+
 	while (true)
 	{
 		if (licznik == 0)
 		{
-			licznik = (rand() % 2)+1;
+			licznik = (rand() % 2) + 1;
 			tworzenieSamolotu(samoloty, liczbaSamolotow);
 		}
 		tura(samoloty, liczbaSamolotow);
